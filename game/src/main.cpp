d9c8a8192b203c2f5cdbf760f14d850ce4c7e238
HakOzcan <hakan.ozcan6217@gmail.com>
2023-05-19 09:24:36 -0400

    Final for Lab_1

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,103 +1,111 @@
 //HELLO SUNSHINE
 #include "rlImGui.h"
-#include "Physics.h"
-#include "Collision.h"
-#include "Math.h"
 
-#include <array>
-#include <vector>
-#include <string>
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
-//Vector2 ChangePosition(const Vector2& position1, const Vector2& position2)
-//{
-//    return position1 + position2;
-//}
-
 int main(void)
 {
+    //INIT//
     const int screenWidth = 1920;
     const int screenHeight = 1080;
     InitWindow(screenWidth, screenHeight, "Sunshine");
+    InitAudioDevice();//Initiliaze the audio device to be able to play sound, music etc.
 
+    //LOAD//
+    //Load Player..
     float playerRotation = 0.0f;
     const float playerSpriteWidth = 32.0f;
     const float playerSpriteHeight = 32.0f;
-    const float playerSpeed = 500.0f;
-    const float playerRotationSpeed = 100.0f;
-
-    const char* playerSpriteFilePath = "../game/assets/textures/New Piskel (2).png";
-    Texture2D playerSprite = LoadTexture(playerSpriteFilePath);
-
+    const float playerRotationSpeed = 200.0f;
+    Texture2D playerSprite = LoadTexture("../game/assets/textures/New Piskel (2).png");//Load 2d texture into GPU memory
     Vector2 playerPosition = { 0.0f, 0.0f };
-    //const Vector2 playerDirection = Direction(playerRotation * DEG2RAD);
     const Rectangle playerRecSrc{ 0, 0, playerSpriteWidth, playerSpriteHeight };
     const Vector2 playerOrigin{ playerPosition.x + (playerSpriteWidth * 2), playerPosition.y + (playerSpriteHeight * 2)};
-
-    const char* backgroundSpriteFilePath = "../game/assets/textures/Castle1.png";
-    Texture2D backgroundSprite = LoadTexture(backgroundSpriteFilePath);
+    //Load Background..
+    Texture2D backgroundSprite = LoadTexture("../game/assets/textures/Castle1.png");//Load 2d texture into GPU memory
     const float backgroundSpriteWidth = 1280.0f;
     const float backgroundSpriteHeight = 500.0f;
-    //const Vector2 backgroundPosition{ screenWidth / 2, screenHeight / 2 };
     const Vector2 backgroundOrigin{ screenWidth / 2, screenHeight / 2 };
     const Rectangle backgroundRecSrc{ 0, 0, backgroundSpriteWidth, backgroundSpriteHeight };
     const Rectangle backgroundRecDst{ screenWidth / 2, screenHeight / 2, screenWidth, screenHeight };
+    //Load Music..
+    Music mainThemeMusic = LoadMusicStream("../game/assets/audio/Main theme.mp3");//Enable us to load music file to the buffer
+    Music darkThemeMusic = LoadMusicStream("../game/assets/audio/Dark theme.ogg");//Enable us to load music file to the buffer
 
+    //SET FPS//
     SetTargetFPS(60);
+    //GAME LOOP//
     while (!WindowShouldClose())
     {
+        //Play and update main music..
+        PlayMusicStream(mainThemeMusic);//Stream the music which was loaded into buffer before
+        UpdateMusicStream(mainThemeMusic);//Update the buffer to play continuously music
 
+        //GET DELTA TIME//
         float dt = GetFrameTime();
-        if (IsKeyDown(KEY_E))
-            playerRotation += playerRotationSpeed * dt;
-        if (IsKeyDown(KEY_Q))
-            playerRotation -= playerRotationSpeed * dt;
-        //if (IsKeyDown(KEY_D))
-        //    playerPosition.x += playerSpeed * dt;
-        //if (IsKeyDown(KEY_A))
-        //    playerPosition.x -= playerSpeed * dt;
-        //if (IsKeyDown(KEY_W))
-        //    playerPosition.y -= playerSpeed * dt;
-        //if (IsKeyDown(KEY_S))
-        //    playerPosition.y += playerSpeed * dt;
 
-        playerPosition = GetMousePosition();
+        //TAKE INPUT//
+        if (IsKeyDown(KEY_E))//Repeatedly takes input from keyboard while key pressed and held
+            playerRotation += playerRotationSpeed * dt;
+        if (IsKeyDown(KEY_Q))//Repeatedly takes input from keyboard while key pressed and held
+            playerRotation -= playerRotationSpeed * dt;
+
+        //Update player position according to mouse position..
+        playerPosition = GetMousePosition();//Provides mouse current position as Vector2(x and y)
         const Rectangle playerRecDst{ playerPosition.x, playerPosition.y, playerSpriteWidth * 4, playerSpriteHeight * 4 };
+
+        //Variables related to player and stationary circle
         const Vector2 playerCircleOrigin{ playerPosition.x, playerPosition.y};
-        const Vector2 stationaryCircleOrigin{ 1600.0f, 200.0f };
         const float circleRadius = playerRecDst.width / 2;
+        const Vector2 stationaryCircleOrigin{ 1600.0f, 200.0f };
         Color playerCircleColor = WHITE;
         Color stationaryCircleColor = YELLOW;
         Color backgroundColor = WHITE;
 
-        if (CheckCollisionCircles(playerCircleOrigin, circleRadius, stationaryCircleOrigin, circleRadius))
+        //COLLISION//
+        if (CheckCollisionCircles(playerCircleOrigin, circleRadius, stationaryCircleOrigin, circleRadius))//Checks collision between two circles and return boolean
         {
             playerCircleColor = BLACK;
-            stationaryCircleColor = YELLOW;
-                if (playerCircleOrigin.x == stationaryCircleOrigin.x || playerCircleOrigin.y == stationaryCircleOrigin.y)
-                { 
+            if (playerCircleOrigin.x == stationaryCircleOrigin.x || playerCircleOrigin.y == stationaryCircleOrigin.y)//Checks if the circles completely overlap, changes music and colors accorcdingly
+            {
                 backgroundColor = BLACK;
                 stationaryCircleColor = BLACK;
-                }
+                PauseMusicStream(mainThemeMusic);//Pause the streaming music
+                PlayMusicStream(darkThemeMusic);//Stream the music which was loaded into buffer before
+                UpdateMusicStream(darkThemeMusic);//Update the buffer to play continuously music
+            }
+            else
+            {
+                backgroundColor = WHITE;
+                stationaryCircleColor = YELLOW;
+                playerCircleColor = BLACK;
+                StopMusicStream(darkThemeMusic);//Stop playing music
+                ResumeMusicStream(mainThemeMusic);//Resume playing the music where it left
+            }
+
         }
 
+        //DRAW//
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawTexturePro(backgroundSprite, backgroundRecSrc, backgroundRecDst, backgroundOrigin, NULL, backgroundColor);
-        DrawCircleV(playerCircleOrigin, circleRadius, playerCircleColor);
-        DrawCircleV(stationaryCircleOrigin, circleRadius, stationaryCircleColor);
-        // Render player
-        DrawTexturePro(playerSprite, playerRecSrc, playerRecDst, playerOrigin, playerRotation, YELLOW);
-
+        DrawTexturePro(backgroundSprite, backgroundRecSrc, backgroundRecDst, backgroundOrigin, NULL, backgroundColor);//Draw a texture defined by a rectangle with detailed parameters
+        DrawCircleV(stationaryCircleOrigin, circleRadius, stationaryCircleColor);//Draw a basic circle based on origin, circle and color
+        DrawCircleV(playerCircleOrigin, circleRadius, playerCircleColor);//Draw a basic circle based on origin, circle and color
+        DrawTexturePro(playerSprite, playerRecSrc, playerRecDst, playerOrigin, playerRotation, YELLOW);//Draw a texture defined by a rectangle with detailed parameters
 
 
         EndDrawing();
     }
 
+    //CLEAN//
+    UnloadTexture(playerSprite);//Unload the 2D texture from VRAM before closing the window
+    UnloadTexture(backgroundSprite);//Unload the 2D texture from VRAM before closing the window
+    UnloadMusicStream(darkThemeMusic);//Unload the music file from RAM before closing the window
+    UnloadMusicStream(mainThemeMusic);//Unload the music file from RAM before closing the window
+    CloseAudioDevice();//Close the audio device which was initiliazed at the beginning
     CloseWindow();
 
     return 0;
