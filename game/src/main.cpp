f038a33c765d48469f6c70e1bcd26348cde8729d
HakOzcan <hakan.ozcan6217@gmail.com>
2023-06-01 13:18:59 -0400

    Ready for Lab2

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -23,8 +23,8 @@ float GetMagnitude(Vector2 vector)
 
 class Rigidbody
 {
-protected:
-    Vector2 objectPos;
+public:
+    Vector2 objectPos{ 0.0f, 0.0f };
     Vector2 objectVel{ 50.0f, 0.0f };
     Vector2 objectAccel{ 0.0f, 100.0f };
     const float kCircleRadius { 50.0f };
@@ -32,30 +32,50 @@ protected:
 public:
     Rigidbody() {};
 
+    Rigidbody(Vector2 objectPos, Vector2 objectVel, Vector2 objectAccel)
+    {
+        this->objectPos = objectPos, this->objectVel = objectVel, this->objectAccel = objectAccel; 
+    }
+
+    void SetPosition(Vector2 pos)
+    {
+        objectPos = pos;
+    }
+
+    void SetPositionX(float posx)
+    {
+        objectPos.x = posx;
+    }
+    void SetPositionY(float posy)
+    {
+        objectPos.y = posy;
+    }
+
+
      Vector2 GetPosition() { return objectPos; }
      Vector2 GetVelocity() { return objectVel; }
      Vector2 GetAcceleration() { return objectAccel; }
      float GetCircleRadius() { return kCircleRadius; }
 
-     void SetPosition(Vector2 pos)
+     void Update(const float deltaTime)//Vector2& pos, Vector2& vel, Vector2 accel, const float deltaTime
      {
-         objectPos = pos;
-     }
+         objectPos = objectPos + (objectVel * deltaTime) + (objectAccel * 0.5f * deltaTime * deltaTime);
 
-     void Update(Vector2 vel, const float deltaTime)//Vector2& pos, Vector2& vel, Vector2 accel, const float deltaTime
-     {
-         objectPos = objectPos + (vel * deltaTime) + (objectAccel * 0.5f * deltaTime * deltaTime);
+         objectVel = objectVel + (objectAccel * deltaTime);
+
+         objectAccel = { 0 };
      }
 };
 
-class Agent : public Rigidbody
+class Agent
 {
-private:
-    float maxSpeed{ 2000.0f };
-    float maxAccel{ 2000.0f };
-    Rigidbody Object;
+public:
+    float maxSpeed = 2000.0f;
+    float maxAccel = 2000.0f;
     
 public:
+    Rigidbody object;
+
     Agent() {};
 
     Agent(float maxSpeed, float maxAccel)
@@ -67,23 +87,33 @@ public:
     {
         return maxSpeed;
     }
-
-    static Vector2 Seek(const Vector2& playerPos, const Vector2& agentPos, const Vector2& agentVel, float maxSpeed, float maxAccel)
+    float GetMaxAccel()
     {
-        Vector2 toPlayer = Normalize(playerPos - agentPos);
-        Vector2 desiredVel = toPlayer * maxSpeed;
-        Vector2 deltaVel = desiredVel - agentVel;
-        Vector2 accel = Normalize(deltaVel) * maxAccel;
-
-        return accel;
+        return maxAccel;
     }
-
-    static Vector2 Flee(const Vector2& playerPos, const Vector2& agentPos, const Vector2& agentVel, float maxSpeed, float maxAccel)
-    {
-        return Negate(Seek(playerPos, agentPos, agentVel, maxSpeed, maxAccel));
-    }
-
 };
+
+Vector2 Seek(const Vector2& targetPos, const Vector2& agentPos, Vector2 agentVel, float maxSpeed, float maxAccel)
+{
+    Vector2 toPlayer = Normalize(targetPos - agentPos);
+    Vector2 desiredVel = toPlayer * maxSpeed;
+    Vector2 deltaVel = desiredVel - agentVel;
+    Vector2 accel = Normalize(deltaVel) * maxAccel;
+    //agentVel = agentVel + accel;
+
+    return accel;
+}
+
+Vector2 Flee(const Vector2& targetPos, const Vector2& agentPos, Vector2 agentVel, float maxSpeed, float maxAccel)
+{
+    Vector2 toPlayer = Normalize(agentPos - targetPos);
+    Vector2 desiredVel = toPlayer * maxSpeed;
+    Vector2 deltaVel = desiredVel - agentVel;
+    Vector2 accel = Normalize(deltaVel) * maxAccel;
+    //agentVel = agentVel + accel;
+
+    return accel;
+}
 
 int main(void)
 {
@@ -91,41 +121,58 @@ int main(void)
     //rlImGuiSetup(true);
     SetTargetFPS(240);
 
-    Agent a1 = Agent(3000.0f, 3000.0f);
-    a1.SetPosition({ 100.0f, 100.0f });
-    Agent a2 = Agent(2000.0f, 2000.0f);
-    a2.SetPosition({ 200.0f, 200.0f });
-    Agent a3 = Agent(1500.0f, 1500.0f);
-    a3.SetPosition({ 300.0f, 300.0f });
+    Agent a1 = Agent(1500.0f, 1500.0f);
+    a1.object.SetPosition({ 100.0f, 100.0f });
+    Agent a2 = Agent(1250.0f, 1250.0f);
+    a2.object.SetPosition({ 200.0f, 200.0f });
+    Agent a3 = Agent(1000.0f, 1000.0f);
+    a3.object.SetPosition({ 300.0f, 300.0f });
 
-    std::vector<Rigidbody*> pObjects;
+    std::vector<Agent> agents;
+    
+    agents.push_back(a1);
+    agents.push_back(a2);
+    agents.push_back(a3);
 
-    pObjects.push_back(&a1);
-    pObjects.push_back(&a2);
-    pObjects.push_back(&a3);
+    std::vector<Rigidbody> pObjects;
 
-    Vector2 playerPos{ 0.0f, 0.0f };
+    pObjects.push_back(a1.object);
+    pObjects.push_back(a2.object);
+    pObjects.push_back(a3.object);
+
+    Rigidbody o1 = Rigidbody({ 800.0f, 800.0f }, { 0.0f, 0.0f }, { 0.0f, 0.0f });
+    Rigidbody o2 = Rigidbody({ 1000.0f, 800.0f }, { 0.0f, 0.0f }, { 0.0f, 0.0f });
+    Rigidbody o3 = Rigidbody({ 1500.0f, 800.0f }, { 0.0f, 0.0f }, { 0.0f, 0.0f });
+
+    std::vector<Rigidbody> obstacles;
+
+    obstacles.push_back(o1);
+    obstacles.push_back(o2);
+    obstacles.push_back(o3);
+
 
     while (!WindowShouldClose())
     {
-        const float deltaTime = GetFrameTime();
-        playerPos = GetMousePosition();
+        float deltaTime = GetFrameTime();
+        Vector2 playerPos = GetMousePosition();
 
+        for (int i = 0; i < pObjects.size(); i++)
+        {
+         pObjects[i].objectAccel = Flee(obstacles[i].GetPosition(), pObjects[i].GetPosition(), pObjects[i].GetVelocity(), agents[i].maxSpeed * 0.5f, agents[i].maxAccel * 0.5f);
+         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+         {
+             //Seek(playerPos, pObjects[i].GetPosition(), pObjects[i].GetVelocity(), agents[i].maxSpeed, agents[i].maxAccel);
+             pObjects[i].objectAccel = pObjects[i].objectAccel + Seek(playerPos, pObjects[i].GetPosition(), pObjects[i].GetVelocity(), agents[i].maxSpeed, agents[i].maxAccel);
+         }
+        }
+        
 
-        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+        for (int i = 0; i < pObjects.size(); i++)
         {
-            for (int i = 0; i < pObjects.size(); i++)
-            {
-                pObjects[i]->Update(Agent::Seek(playerPos, pObjects[i]->GetPosition(), pObjects[i]->GetVelocity(), 500.0f, 500.0f), deltaTime);
-            }
+            pObjects[i].Update(deltaTime);
+            WraparoundScreen(pObjects[i].objectPos);
         }
-        else
-        {
-            for (int i = 0; i < pObjects.size(); i++)
-            {
-                pObjects[i]->Update(Agent::Flee(playerPos, pObjects[i]->GetPosition(), pObjects[i]->GetVelocity(), 500.0f, 500.0f), deltaTime);
-            }
-        }
+
 
 
         BeginDrawing();
@@ -141,7 +188,8 @@ int main(void)
         //DrawLineV(agentPos, agentAccel, GREEN);
         for (int i = 0; i < pObjects.size(); i++)
         {
-            DrawCircleV(WraparoundScreen(pObjects[i]->GetPosition()), pObjects[i]->GetCircleRadius(), RED);
+            DrawCircleV(WraparoundScreen(pObjects[i].GetPosition()), pObjects[i].GetCircleRadius(), RED);
+            DrawCircleV(obstacles[i].GetPosition(), obstacles[i].GetCircleRadius(), YELLOW);
         }
 
         DrawCircleV(playerPos, 50, BLUE);
